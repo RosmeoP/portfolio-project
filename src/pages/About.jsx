c78c1a037de249bda39d66344d037e4e36e8b8dd
Mauricio <vrosmeo@gmail.com>
2025-01-24 19:59:39 -0600

    Ading the about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,9 +5,19 @@ const About = () => {
     <div className="min-h-screen bg-[#f8f8f8]">
       <NavBar />
       <div className="pt-20">
-        <h1 className='bg-black text-white flex justify-center text-center font-bold text-2xl p-4'>
-          Hello About
+      <div className="flex items-center fixed left-0 h-screen z-50">
+        <h1 
+          className="text-lg text-gray-300 ml-5  font-bold text-center mb-8"
+          style={{ writingMode: 'vertical-rl', textOrientation: 'sideways-lr', transform: 'rotate(180deg)' }}
+        >
+          About 
         </h1>
+      </div>
+        <div className="container mx-auto px-6">
+          <p className="text-black text-center mt-20 max-w-md font-bold text-2xl ">
+          Passionate software engineer focused on building innovative systems and solving complex problems with impactful solutions.          </p>
+         
+      </div>
       </div>
     </div>
   )
