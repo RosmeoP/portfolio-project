e2d873ef0cbf04bd8b4f8d8cd64d59eb756e1045
Mauricio <vrosmeo@gmail.com>
2025-01-29 18:38:10 -0600

    changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import emailjs from 'emailjs-com';
 import NavBar from '../components/NavBar';
-import { Alert, AlertTitle, AlertDescription } from "../components/ui/Alert";
+import { Alert, AlertTitle, AlertDescription } from "@/components/ui/Alert";
 import { X } from 'lucide-react';
 
 const ContactForm = () => {
